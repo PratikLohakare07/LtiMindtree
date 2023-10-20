b55f059c69b81f40920f7ed89d4e062fc680de36
Pratik Lohakare <pratiklohakare07@gmail.com>
2023-10-20 11:26:47 +0000

    commit

---
+++ Entity/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Entity;
+public class Movie
+{
+     public int Id{get;set;}
+     public string Name{get;set;}
+
+     public int Ratings{get;set;}
+
+     public int Ryear{get;set;}
+
+}
+
+
